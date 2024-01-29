9bba7626caec8b52967f0a66fe85bc829eb7fdd5
Fabricio Rojas <fabrijob99@gmail.com>
2024-01-29 16:45:23 -0600

    Unzip moved after exe selection

--- PSInstall.ps1
+++ PSInstall.ps1
@@ -1,6 +1,7 @@
 <# Possible platform detection to decide on specific 
 installation options and parallel installation #>
 
+
 <# Ask for offline or online installation(I'll probably get rid of online 
 installation as a whole. WinGet already exists for this and, 
 if you're already in the command line, why would you use a wrapper for WinGet?) #>
@@ -9,14 +10,14 @@ $InsType = Read-Host "Enter 'on' for offline installation or 'off' for offline i
     <#Network checkup for online installation if failed, 
     ask to default to offline#>
 
-# Package file unzip(Remove this if compression is negligible)
-
 <# Executables listing. Listed packages are assigned numbers and
 user prompted to enter numbers for packages to be installed or A for all. #>
 (Get-ChildItem -Path ./Packages -Force -Recurse -Include *.exe,*.zip).BaseName |
     Where-Object -FilterScript {
         ()
     }
+
+# Package file unzip(Remove this if compression is negligible)
 
 <# Default or insert installation path#>
 
